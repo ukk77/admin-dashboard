4d76bbf1a8e8249f76b9b89ee330df6e074aed99
Uddesh Karda <ukarda@gmail.com>
2019-06-28 16:15:41 +0530

    Add files via upload
    client build folder

---
+++ client/build/precache-manifest.19bea2199e88ed6defe4c9b9363aebda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fcb14d6ebf0b33623441186011d534",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27a8efd2d04855c7446",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0da6ffd0e4aa18582ae2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f27a8efd2d04855c7446",
+    "url": "/static/js/2.e7a914d3.chunk.js"
+  },
+  {
+    "revision": "0da6ffd0e4aa18582ae2",
+    "url": "/static/js/main.d767643e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
